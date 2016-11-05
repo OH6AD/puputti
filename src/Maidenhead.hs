0bc9c06ef9974cc7e526b25985a5075ca3a63a0e
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2016-11-05 14:56:55 +0200

    Support calculating distance of given points in Maidenhead

--- src/Maidenhead.hs
+++ src/Maidenhead.hs
@@ -1,4 +1,8 @@
-module Maidenhead (maidenheadToWgs84) where
+module Maidenhead ( maidenheadToWgs84
+                  , maidenheadToWgs84rad
+                  , distance
+                  , maidenheadDistance
+                  ) where
 
 import Data.Char (toUpper, ord)
 import Data.List (transpose)
@@ -26,10 +30,33 @@ charToFloat ((min,max),x') (Just acc)
         x = toUpper x'
 
 -- |Converts Maidenhead coordinate string to latitude and longitude in
--- WGS84 degrees.
-maidenheadToWgs84 :: (Fractional a) => String -> Maybe (a, a)
-maidenheadToWgs84 s = latLon <$> mapM toPos (transpose $ chunksOf 2 s)
+-- WGS84 (in semicircles i.e. 180°)
+maidenheadToWgs84sc :: (Fractional a) => String -> Maybe (a, a)
+maidenheadToWgs84sc s = latLon <$> mapM toPos (transpose $ chunksOf 2 s)
    where
-     latLon [lonRaw, latRaw] = ((latRaw-0.5)*180, (lonRaw-0.5)*360)
+     latLon [lonRaw, latRaw] = ((latRaw-0.5), (lonRaw-0.5)*2)
      toPos = foldr charToFloat middle . zip alphabet
      middle = Just (1/2) -- 0.5 gets the centre of a square
+
+-- |Converts Maidenhead coordinate string to latitude and longitude in
+-- WGS84 degrees.
+maidenheadToWgs84 :: Fractional t => String -> Maybe (t, t)
+maidenheadToWgs84 s = (\(a,b) -> (180*a,180*b)) <$> maidenheadToWgs84sc s
+
+-- |Converts Maidenhead coordinate string to latitude and longitude in
+-- WGS84 radians.
+maidenheadToWgs84rad :: Floating t => String -> Maybe (t, t)
+maidenheadToWgs84rad s = (\(a,b) -> (pi*a,pi*b)) <$> maidenheadToWgs84sc s
+
+-- |Calculate distance in kilometres between two points (in
+-- radians) using Haversine formula
+-- https://en.wikipedia.org/wiki/Haversine_formula
+distance (φ1, λ1) (φ2, λ2) = d * asin (sqrt (sin² ((φ2-φ1)/2) + cos φ1 * cos φ2 * sin² ((λ2-λ1)/2)))
+  where d = 12735 -- Earth's diameter
+        sin² = (**2) . sin
+
+-- |Calculates distance in kilometres between two points in Maidenhead
+maidenheadDistance :: Floating a => String -> String -> Maybe a
+maidenheadDistance a b = distance <$>
+                         maidenheadToWgs84rad a <*>
+                         maidenheadToWgs84rad b
